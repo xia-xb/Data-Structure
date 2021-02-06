3764b2807f57a9273b4519c2ffcefca4d066bd58
xia-xb <2276905732@qq.com>
2021-02-06 21:50:45 +0800

    format

--- src/dulist.cpp
+++ src/dulist.cpp
@@ -2,60 +2,57 @@
 
 #include <iostream>
 
-Dulist::Dulist(){
-    this->data=0;
-    this->length=0;
-    this->pre=this;
-    this->next=this;
+Dulist::Dulist() {
+    this->data = 0;
+    this->length = 0;
+    this->pre = this;
+    this->next = this;
 }
 
-Dulist::~Dulist(){
+Dulist::~Dulist() {}
 
-}
-
-bool Dulist::Insert(int i,ElemType &e){
-    if(i<=0||i>this->length+1){
+bool Dulist::Insert(int i, ElemType &e) {
+    if (i <= 0 || i > this->length + 1) {
         return 0;
     }
-    Dulist *p=this;
-    for(int j=1;j<=i-1;j++){
-        p=(*p).next;
+    Dulist *p = this;
+    for (int j = 1; j <= i - 1; j++) {
+        p = (*p).next;
     }
-    Dulist *q=new(Dulist);
-    (*q).data=e;
-    (*q).next=(*p).next;
-    (*q).pre=p;
-    p->next->pre=q;
-    p->next=q;
+    Dulist *q = new (Dulist);
+    (*q).data = e;
+    (*q).next = (*p).next;
+    (*q).pre = p;
+    p->next->pre = q;
+    p->next = q;
     this->length++;
     return 1;
 }
 
-bool Dulist::Delete(int i,ElemType &e){
-    if(i<=0||i>this->length){
+bool Dulist::Delete(int i, ElemType &e) {
+    if (i <= 0 || i > this->length) {
         return 0;
     }
-    Dulist *p=this;
-    for(int j=1;j<=i;j++){
-        p=(*p).next;
+    Dulist *p = this;
+    for (int j = 1; j <= i; j++) {
+        p = (*p).next;
     }
-    e=(*p).data;
-    p->pre->next=p->next;
-    p->next->pre=p->pre;
-    delete(p);
+    e = (*p).data;
+    p->pre->next = p->next;
+    p->next->pre = p->pre;
+    delete (p);
     this->length--;
     return 1;
 }
 
-bool Dulist::getElem(int i,ElemType &e){
-    if(i<=0||i>this->length||this->length==0){
+bool Dulist::getElem(int i, ElemType &e) {
+    if (i <= 0 || i > this->length || this->length == 0) {
         return 0;
     }
-    Dulist *p=this;
-    for(int j=1;j<=i;j++){
-        p=(*p).next;
+    Dulist *p = this;
+    for (int j = 1; j <= i; j++) {
+        p = (*p).next;
     }
-    e=(*p).data;
+    e = (*p).data;
     return 1;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -6,88 +6,88 @@ using namespace std;
 
 int main() {
     Dulist list;
-    cout<<"链表的长度："<<list.length<<endl;
+    cout << "链表的长度：" << list.length << endl;
 
-    cout<<endl<<"依次插入10个元素"<<endl;
-    for(int i=1;i<=10;i++){
-        if(list.Insert(i,i)&&i==10){
-            cout<<"插入元素成功"<<endl;
+    cout << endl << "依次插入10个元素" << endl;
+    for (int i = 1; i <= 10; i++) {
+        if (list.Insert(i, i) && i == 10) {
+            cout << "插入元素成功" << endl;
         }
     }
-    cout<<"打印元素"<<endl;
-    cout<<"链表的长度："<<list.length<<endl;
-    for(int i=1;i<=list.length;i++){
+    cout << "打印元素" << endl;
+    cout << "链表的长度：" << list.length << endl;
+    for (int i = 1; i <= list.length; i++) {
         ElemType e;
-        if(list.getElem(i,e)){
-            cout<<"第"<<i<<"个元素为："<<e;
+        if (list.getElem(i, e)) {
+            cout << "第" << i << "个元素为：" << e;
         }
-        cout<<endl;
-    }
-
-    cout<<"在链表第5个元素位置插入20"<<endl;
-    ElemType a=20;
-    if(list.Insert(5,a)){
-        cout<<"插入元素成功"<<endl;
-    }else{
-        cout<<"插入元素失败"<<endl;
-    }
-    cout<<"打印元素"<<endl;
-    cout<<"链表的长度："<<list.length<<endl;
-    for(int i=1;i<=list.length;i++){
-        ElemType e;
-        if(list.getElem(i,e)){
-            cout<<"第"<<i<<"个元素为："<<e;
-        }
-        cout<<endl;
+        cout << endl;
     }
 
-    cout<<"在链表第15个元素位置插入40"<<endl;
-    a=40;
-    if(list.Insert(15,a)){
-        cout<<"插入元素成功"<<endl;
-    }else{
-        cout<<"插入元素失败"<<endl;
+    cout << "在链表第5个元素位置插入20" << endl;
+    ElemType a = 20;
+    if (list.Insert(5, a)) {
+        cout << "插入元素成功" << endl;
+    } else {
+        cout << "插入元素失败" << endl;
     }
-    cout<<"打印元素"<<endl;
-    cout<<"链表的长度："<<list.length<<endl;
-    for(int i=1;i<=list.length;i++){
+    cout << "打印元素" << endl;
+    cout << "链表的长度：" << list.length << endl;
+    for (int i = 1; i <= list.length; i++) {
         ElemType e;
-        if(list.getElem(i,e)){
-            cout<<"第"<<i<<"个元素为："<<e;
+        if (list.getElem(i, e)) {
+            cout << "第" << i << "个元素为：" << e;
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    cout<<"删除链表中第3个元素"<<endl;
-    if(list.Delete(3,a)){
-        cout<<"删除元素成功"<<endl;
-    }else{
-        cout<<"删除元素失败"<<endl;
+    cout << "在链表第15个元素位置插入40" << endl;
+    a = 40;
+    if (list.Insert(15, a)) {
+        cout << "插入元素成功" << endl;
+    } else {
+        cout << "插入元素失败" << endl;
     }
-    cout<<"打印元素"<<endl;
-    cout<<"链表的长度："<<list.length<<endl;
-    for(int i=1;i<=list.length;i++){
+    cout << "打印元素" << endl;
+    cout << "链表的长度：" << list.length << endl;
+    for (int i = 1; i <= list.length; i++) {
         ElemType e;
-        if(list.getElem(i,e)){
-            cout<<"第"<<i<<"个元素为："<<e;
+        if (list.getElem(i, e)) {
+            cout << "第" << i << "个元素为：" << e;
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    cout<<"删除链表中第13个元素"<<endl;
-    if(list.Delete(13,a)){
-        cout<<"删除元素成功"<<endl;
-    }else{
-        cout<<"删除元素失败"<<endl;
+    cout << "删除链表中第3个元素" << endl;
+    if (list.Delete(3, a)) {
+        cout << "删除元素成功" << endl;
+    } else {
+        cout << "删除元素失败" << endl;
     }
-    cout<<"打印元素"<<endl;
-    cout<<"链表的长度："<<list.length<<endl;
-    for(int i=1;i<=list.length;i++){
+    cout << "打印元素" << endl;
+    cout << "链表的长度：" << list.length << endl;
+    for (int i = 1; i <= list.length; i++) {
         ElemType e;
-        if(list.getElem(i,e)){
-            cout<<"第"<<i<<"个元素为："<<e;
+        if (list.getElem(i, e)) {
+            cout << "第" << i << "个元素为：" << e;
         }
-        cout<<endl;
+        cout << endl;
+    }
+
+    cout << "删除链表中第13个元素" << endl;
+    if (list.Delete(13, a)) {
+        cout << "删除元素成功" << endl;
+    } else {
+        cout << "删除元素失败" << endl;
+    }
+    cout << "打印元素" << endl;
+    cout << "链表的长度：" << list.length << endl;
+    for (int i = 1; i <= list.length; i++) {
+        ElemType e;
+        if (list.getElem(i, e)) {
+            cout << "第" << i << "个元素为：" << e;
+        }
+        cout << endl;
     }
     return 0;
 }
