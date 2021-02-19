1a8b0b3051bfec931775ca5ec70755c4512d02b3
xia-xb <2276905732@qq.com>
2021-02-19 21:29:52 +0800

    commit KMP of string

--- src/String.cpp
+++ src/String.cpp
@@ -22,3 +22,55 @@ int BF(String s, String t, int pos) {
         return 0;
     }
 }
+
+void get_next(String t, int *next) {
+    int i = 1, j = 0;
+    next[1] = 0;
+    while (i < t[0]) {
+        if (j == 0 || t[i] == t[j]) {
+            i++;
+            j++;
+            next[i] = j;
+        } else {
+            j = next[j];
+        }
+    }
+}
+
+void get_nextval(Stirng t, int *nextval) {
+    int i = 1, j = 0;
+    nextval[1] = 0;
+    while (i < t[0]) {
+        if (j == 0 || t[i] == t[j]) {
+            i++;
+            j++;
+            if (t[i] != t[i]) {
+                nextval[i] = j;
+            } else {
+                nextval[i] = nextval[j];
+            }
+        } else {
+            j = nextval[j];
+        }
+    }
+}
+
+int Index_KMP(String s, String t, int pos) {
+    int i = pos;
+    int j = 1;
+    int next[255];
+    get_next(t, next);
+    while (i < s[0] && j < t[0]) {
+        if (j == 0 || s[i] == t[j]) {
+            i++;
+            j++;
+        } else {
+            j = next[j];
+        }
+    }
+    if (j > t[0]) {
+        return i - t[0];
+    } else {
+        return 0;
+    }
+}
